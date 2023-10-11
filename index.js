b62b8506f89d7e3c59ebf8e0c88b23ec2d164a35
emiltog@gmail.com <emiltog@gmail.com>
2023-10-11 13:33:38 -0500

    Add support for Enter key press and scroll to top the test results

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// Check messages 
 const NORMAL = "<h3 class='green-text'>Normal</h3><p>Your weight is normal. Keep on the watch and take care of yourself.</p>";
 
 const UNDERWEIGHT = "<h3 class='yellow-text'>Underweight</h3><p>You are underweight. Hence, you should take care of your deeds.</p>";
@@ -5,12 +7,26 @@ const OVERWEIGHT = "<h3 class='yellow-text'>Overweight</h3><p>You are overweight
 
 const OBESITY = "<h3 class='red-text'>Obesity</h3><p>Your are at risk of heart disease because of your overweight status. You should act promptly to control your foods, do a physical exercise regularly and change your daily routine.</p>"
 
+//Defining DOM elements with jQuery
 const $weightInput = $("#weight-input");
 const $heightInput = $("#height-input");
 const $result = $("#result");
 const $calculateBtn = $("#calculate-bmi");
 
-$calculateBtn.on("click", calculate);
+$calculateBtn.on("click", checker);
+
+// Execute a function when the user presses a key on the keyboard
+$heightInput.on("keypress", enterCheck);
+
+function enterCheck(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        $calculateBtn.click();
+    }
+}
 
 function bmiCalculator(weight, height) {
     if (weight <= 0 || height <= 0) {
@@ -23,10 +39,10 @@ function bmiCalculator(weight, height) {
     }
 }
 
-function calculate() {
+// Check result 
+function checker() {
     let weight = $weightInput.val();
     let height = $heightInput.val();
-    console.log(weight, height);
     let bmi = bmiCalculator(weight, height);
 
     if (bmi > 0) {
@@ -35,6 +51,9 @@ function calculate() {
             bmi >= 25 && bmi < 30 ? OVERWEIGHT :
             OBESITY;
         $result.html(testResult);
+        
+//scroll the results to top
+$(window).scrollTop($result.position().top)
     }
     else if (bmi <= 0) {
         $result.html("Check your Input and Try again");
